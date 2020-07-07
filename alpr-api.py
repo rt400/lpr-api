ebfb48f7f1825a05790f36cf37be6dc9322df54a
Yuval <38878996+rt400@users.noreply.github.com>
2020-07-07 07:33:56 +0300

    Update alpr-api.py

--- alpr-api.py
+++ alpr-api.py
@@ -29,4 +29,4 @@ class alprAPI(Resource):
 api.add_resource(alprAPI, '/<string:todo_id>')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=3000)
+    app.run(debug=True, host='0.0.0.0', port=3370)
